04e866c5b71f16776e6d463f7e33b89a8f300495
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-09-06 07:34:04 +0300

    Redesign class methods

--- photos/models.py
+++ photos/models.py
@@ -4,12 +4,20 @@ from django.db import models
 
 
 class Location(models.Model):
-    name = models.CharField(max_length =80, unique='True')
-    
-    class Meta:
-        ordering = ["name"]
-        verbose_name = "Location"
-        verbose_name_plural = "Locations"        
+    name = models.CharField(max_length =80)
+
+
+    @classmethod
+    def get_locations(cls):
+        locations = cls.objects.all()
+        return locations
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)       
     
     def save_location(self):
         self.save()    
@@ -17,22 +25,18 @@ class Location(models.Model):
     def delete_location(self):
         self.delete()
 
-    @classmethod
-    def get_location(cls):
-        locations = cls.objects.all()
-        return locations
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Location"
+        verbose_name_plural = "Locations" 
     
 
 class Category(models.Model):
-    name = models.CharField(max_length =80, unique='True')
-    
-    class Meta:
-        ordering = ["name"]
-        verbose_name = "Category"
-        verbose_name_plural = "Categories"
+    name = models.CharField(max_length =80)
+
+    def __str__(self):
+        return self.name 
     
     def save_category(self):
             self.save()
@@ -40,18 +44,11 @@ class Category(models.Model):
     def delete_category(self):
         self.delete()
 
-    @classmethod
-    def get_category(cls):
-        categories = cls.objects.all()
-        return categories
 
-    @classmethod
-    def search_by_category(cls, search_term):
-        images = cls.objects.filter(category__category__icontains=search_term)
-        return images
-
-    def __str__(self):
-        return self.name  
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
         
 
 class Image(models.Model):
@@ -61,17 +58,35 @@ class Image(models.Model):
     category = models.ForeignKey(Category, related_name='category', on_delete=models.DO_NOTHING)
     pub_date = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to = 'images/')
-    
-    class Meta:
-        ordering = ["pub_date"]
-        verbose_name = "Image"
-        verbose_name_plural = "Images" 
+
     
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
 
     def summary(self):
         return self.description[:100]+" ......"
+
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
+
+    def __str__(self):
+        return self.name
     
     def save_image(self):
         self.save()
@@ -79,52 +94,8 @@ class Image(models.Model):
     def delete_image(self):
         self.delete()    
 
-    def get_image_by_id(cls):
-        images = cls.objects.get(pk=id)
-        return images
 
-    @classmethod
-    def get_images(cls):
-        images = cls.objects.all()
-        return images
-    
-    def search_image(category):
-        pass
-    
-    def filter_by_location(location):
-        image_location = Image.objects.filter(location__name=location).all()
-        return image_location
-    
-    @classmethod
-    def update_image(cls, id, value):
-        cls.objects.filter(id=id).update(image=value)
-
-    @classmethod
-    def search_by_category(cls,search_term):
-        searched_images = cls.objects.filter(category__category__icontains=search_term)
-        return searched_images
-
-    @classmethod
-    def search_by_location(cls, location):
-        images = cls.objects.filter(location__location=location)
-        return images
-
-    @classmethod
-    def get_by_category(cls, category):
-        images = cls.objects.filter(category__category=category)
-        return images
-
-    @classmethod
-    def get_image(request, id):
-        locations = Location.get_location()
-        try:
-            image = Image.objects.get(pk = id)
-            print(image)
-            
-        except ObjectDoesNotExist:
-            raise Http404()
-        
-        return image
-    
-    def __str__(self):
-        return self.name
+    class Meta:
+        ordering = ["pub_date"]
+        verbose_name = "Image"
+        verbose_name_plural = "Images" 
