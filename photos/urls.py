4bffda3b9f1e8efc833e03b43505850b87e69abc
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-09-06 07:41:37 +0300

    Change urls.py; Made it simple and straightforward

--- photos/urls.py
+++ photos/urls.py
@@ -3,16 +3,10 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns=[
-    url(r'^$',views.index, name = 'home'),
-    url(r'^gallery/$', views.gallery, name = 'gallery'),
-    url(r'^gallery/<int:image_id>/$', views.single_image_details, name='details'),
-    url(r'^search/', views.search_category, name = 'search_category'),
-    url(r'^image/(\d+)', views.get_image, name='image_results'),
-    url(r'^category/urban/$', views.urban, name = 'urban'),
-    url(r'^category/wild/$', views.wild, name = 'wild'),
-    url(r'^category/traditional/$', views.traditional, name = 'traditional'),
-    url(r'^category/scenic/$', views.scenic, name = 'scenic'),
+urlpatterns = [
+    url(r'^$', views.index, name='home'),
+    url(r'^search/', views.search_results, name='search'),
+    url(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
 ]
 
 if settings.DEBUG:
