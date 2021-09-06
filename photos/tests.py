65ff73f74c273568f6a09c2f8ceed535c4607cd8
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-09-06 06:55:41 +0300

    Added tests for each class

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Image, Location, Category
+import datetime as dt
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.loc1= Location(name = 'Nairobi')
+        
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.loc1,Location))
+
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.cat1 = Category(name = 'Sport')
+
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.image1= Location(name = 'Nairobi')
+        self.image1.save_location()
+        
+        self.new_image= Image(name = 'CBD',description = 'This is a random description',location = self.image1)
+        self.new_image.save()
