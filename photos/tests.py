ca2e6f1ceecc9fafaf94a6ec4daf7038d7f5e92b
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-09-06 07:38:35 +0300

    Create/Redesign tests

--- photos/tests.py
+++ photos/tests.py
@@ -1,28 +1,97 @@
 from django.test import TestCase
-from .models import Image, Location, Category
-import datetime as dt
 
-# Create your tests here.
+from .models import Image, Category, Location
 
-class LocationTestClass(TestCase):
-    # Set up method
+
+class TestImage(TestCase):
     def setUp(self):
-        self.loc1= Location(name = 'Nairobi')
-        
-    # Testing  instance
+        self.location = Location(name='test_location')
+        self.location.save_location()
+
+        self.category = Category(name='test_category')
+        self.category.save_category()
+
+        self.image_test = Image(id=1, name='test_image', description='this is a test description', location=self.location,
+                                category=self.category)
+
     def test_instance(self):
-        self.assertTrue(isinstance(self.loc1,Location))
+        self.assertTrue(isinstance(self.image_test, Image))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def test_get_image_by_id(self):
+        found_image = self.image_test.get_image_by_id(self.image_test.id)
+        image = Image.objects.filter(id=self.image_test.id)
+        self.assertTrue(found_image, image)
+
+    def test_search_image_by_location(self):
+        self.image_test.save_image()
+        found_images = self.image_test.filter_by_location(location='test_location')
+        self.assertTrue(len(found_images) == 1)
+
+    def test_search_image_by_category(self):
+        category = 'test_category'
+        found_img = self.image_test.search_by_category(category)
+        self.assertTrue(len(found_img) > 1)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+
+
+class TestLocation(TestCase):
+    def setUp(self):
+        self.location = Location(name='test_location')
+        self.location.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_location(self):
+        self.location.save_location()
+        locations = Location.get_locations()
+        self.assertTrue(len(locations) > 0)
+
+    def test_get_locations(self):
+        self.location.save_location()
+        locations = Location.get_locations()
+        self.assertTrue(len(locations) > 1)
+
+    def test_update_location(self):
+        new_location = 'another_location'
+        self.location.update_location(self.location.id, new_location)
+        changed_location = Location.objects.filter(name='another_location')
+        self.assertTrue(len(changed_location) > 0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
 
 
 class CategoryTestClass(TestCase):
     def setUp(self):
-        self.cat1 = Category(name = 'Wild')
+        self.category = Category(name='test_category')
+        self.category.save_category()
 
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
 
-class ImageTestClass(TestCase):
-    def setUp(self):
-        self.image1= Location(name = 'Nairobi')
-        self.image1.save_location()
-        
-        self.new_image= Image(name = 'CBD',description = 'This is a random description',location = self.image1)
-        self.new_image.save()
+    def test_save_category(self):
+        self.category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_category(self):
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
