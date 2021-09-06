eba88981547e5c2c2584b86c36f5044e1125f645
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-09-06 07:50:50 +0300

    Restructure views.py

--- photos/views.py
+++ photos/views.py
@@ -10,67 +10,28 @@ from django.core.exceptions import ObjectDoesNotExist
 
 def index(request):
     date = dt.date.today()
-    images = Image.get_images()
-    location = Location.get_location()
-    locations = Location.get_location()
-    gallery = Image.objects.all()[:6]
-    return render(request,'index.html', {'gallery':gallery, 'date':date, 'location':location, 'locations':locations, 'images':images})
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request,'index.html', {'images': images[::-1], 'locations': locations, 'date': date})
 
 
-def gallery(request):
-    gallery = Image.objects.all()
-    return render(request, 'gallery/gallery.html', {'gallery':gallery})
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    print(images)
+    return render(request, 'location.html', {'location_images': images})
 
 
-def single_image_details(request,image_id):
-    image_detail = get_object_or_404(Image, pk=image_id)
-    return render(request,'gallery/details.html', {'image_detail':image_detail})
+def search_results(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
 
-
-def search_category(request):
-    if 'category' in request.GET and request.GET["category"]:
-        search_term  =  request.GET.get("category")
-        searched_images = Image.search_by_category(search_term)
-        message  = f"{search_term}"
-        
-        return render(request, 'search.html', {"message":message, "images":searched_images})
+        return render(request, 'search_results.html', {"message": message, "images": searched_images})
 
     else:
-        message = "Please provide a valid search query."
-        
-        return render(request, 'search.html', {"message":message})
+        message = "Please enter a valid search query."
 
-
-def get_image(request, id):
-        locations = Location.get_location()
-        try:
-            image = Image.objects.get(pk = id)
-            print(image)
-            
-        except ObjectDoesNotExist:
-            raise Http404()
-        
-        return render(request, "image.html", {"image":image, "locations":locations})
-
-
-def urban(request):
-    urban_category = Category.objects.get(pk=1)
-    urban = Image.objects.all().filter(category=urban_category)
-    return render(request,'category/urban/urban.html', {"urban":urban})
-
-
-def wild(request):
-    wild_category = Category.objects.get(pk=1)
-    wild = Image.objects.filter(category=wild_category)
-    return render(request,'category/wild/wild.html', {'wild':wild})
-
-
-def traditional(request):
-    traditional_category = Category.objects.get(pk=1)
-    traditional = Image.objects.filter(category=traditional_category)
-    return render(request,'category/traditional/traditional.html', {'traditional':traditional})
-
-def scenic(request):
-    scenic_category = Category.objects.get(pk=1)
-    scenic = Image.objects.filter(category=scenic_category)
-    return render(request,'category/scenic/scenic.html', {'scenic':scenic})
+        return render(request, 'search_results.html', {"message": message})
